bc7cbabdeb880cc36a56f0a2cf4a4e6d667fc3e1
Tony Yip <tony@opensource.hk>
2018-01-20 13:13:19 +0800

    Create sync kernel

---
+++ sync_kernel.go
@@ -0,0 +1,46 @@
+package container
+
+import "sync"
+
+type syncKernel struct {
+	*kernel
+	deferedLock   sync.Locker
+	providersLock sync.Locker
+}
+
+func NewSyncKernel() *syncKernel {
+	base := NewKernel()
+	base.Container = NewSyncContainer()
+
+	return &syncKernel{
+		kernel:        base,
+		deferedLock:   &sync.Mutex{},
+		providersLock: &sync.Mutex{},
+	}
+}
+
+func (kernel *syncKernel) Flush() {
+	kernel.providersLock.Lock()
+	defer kernel.providersLock.Unlock()
+	kernel.kernel.Flush()
+}
+
+func (kernel *syncKernel) Make(abstract string) interface{} {
+	kernel.providersLock.Lock()
+	defer kernel.providersLock.Unlock()
+
+	kernel.deferedLock.Lock()
+	defer kernel.deferedLock.Unlock()
+
+	return kernel.kernel.Make(abstract)
+}
+
+func (kernel *syncKernel) Register(builder ServiceProviderBuilder) {
+	kernel.providersLock.Lock()
+	defer kernel.providersLock.Unlock()
+
+	kernel.deferedLock.Lock()
+	defer kernel.deferedLock.Unlock()
+
+	kernel.kernel.Register(builder)
+}
